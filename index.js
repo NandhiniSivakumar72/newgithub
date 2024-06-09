833b773b1cf505d2d67a845194743484371e4692
NandhiniSivakumar72 <snandhi2020@gmail.com>
2024-06-09 22:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function verifyPass(){
     let pass =document.getElementById('pass').value.length
     if(pass>=8){
-        msg='Good Strength'
+        msg='Strong Strength'
     }else{
-        msg='Poor Strength'
+        msg='Week Strength'
     }
     document.getElementById('strength').innerHTML=msg
 }
